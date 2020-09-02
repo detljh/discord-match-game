32a71af4c8fa56f42f405ea2e67d2a2ca4b49768
lisa <lisa.ng8@hotmail.com>
2020-09-02 17:10:07 +1000

    outline of start and exit game commands

---
+++ commands/exit.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'exit',
+    aliases: ['stop'],
+    description: 'Exits the game you are currently in.',
+    cooldown: 5,
+    execute(message, args, rooms, users) {
+
+    }
+}
---
+++ commands/start.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'start',
+    description: 'Starts the game you are the owner of.',
+    cooldown: 5,
+    execute(message, args, rooms) {
+
+    }    
+}
