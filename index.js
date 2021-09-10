94515044c57293e30cf1e3593f80f51784f3b94e
detljh <lisa.ng8@hotmail.com>
2021-09-11 01:02:07 +1000

    fix to use env vars

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
-const { prefix, token } = require('./config.json'); 
+const prefix = process.env.PREFIX;
+const token = process.env.TOKEN;
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
